44cc10f8af88ca3b57e074ea43f098abb7ec942b
Shihab Mahamud <shihab4t@gmail.com>
2026-01-05 07:03:04 +0600

    welcome

--- 10-image-similarity-search-with-qdrant/app/Http/Controllers/WelcomeController.php
+++ 10-image-similarity-search-with-qdrant/app/Http/Controllers/WelcomeController.php
@@ -2,34 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
-use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
 class WelcomeController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function index(Request $request)
     {
         $imagePaths = Storage::disk('local')->files('cat');
         $imagePaths = Arr::take($imagePaths, 10);
 
-        $base64Images = Collection::make($imagePaths)
-            ->map(fn (string $imagePath) => $this->convertToBase64($imagePath));
+        $selectedImage = null;
+
+        if ($request->has('selected_image')) {
+            $selectedImagePath = $request->input('selected_image');
+            $file = Storage::disk('local')->get($selectedImagePath);
+
+            $selectedImage = [
+                'path' => $selectedImagePath,
+                'base64' => Str::toBase64($file),
+            ];
+        }
+
+        $images = collect($imagePaths)->map(function (string $imagePath) {
+            $file = Storage::disk('local')->get($imagePath);
+
+            return [
+                'path' => $imagePath,
+                'base64' => Str::toBase64($file),
+            ];
+        });
 
         return view('welcome', [
-            'images' => $base64Images,
+            'selectedImage' => $selectedImage,
+            'images' => $images,
         ]);
-
-    }
-
-    public function convertToBase64(string $imagePath)
-    {
-        return $imagePath
-            |> Storage::disk('local')->get(...)
-            |> Str::toBase64(...);
     }
 }
